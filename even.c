823a59ca741cd2d9c740aef39fce49caaa1423f1
kopals123 <48666893+kopals123@users.noreply.github.com>
2019-03-23 13:55:23 +0530

    Update even.c

--- even.c
+++ even.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 void main()
 {
-  int a;
-  scanf("%d",&a);
-  if(a%2==0)
-    printf("Even");
-   else if(a==0)
-    printf("Odd");
-   else
-    printf("invalid");
+  double int a;
+  scanf("%ld",&a);
+  if(a==0)
+      printf("invalid");
+  else if(a%2==0)
+      printf("Even");
+  else
+      printf("Odd");
+      
 }
