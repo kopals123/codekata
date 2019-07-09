f3659b591b8ba5e09819cfe56550fd9274455f10
K AKHIL <akhilkoppala123@gmail.com>
2019-07-09 08:54:34 +0530

    Update h1.py

--- h1.py
+++ h1.py
@@ -1,7 +1,12 @@
 n=input()
 b=[int(m) for m in input().split()]
 d=[]
+flag=0
 for i in set(b):
     if(b.count(i)!=1):
         d.append(i)
-print(*sorted(d))
+        flag=1
+if(flag==1):
+    print(*sorted(d))
+else:
+    print("unique")
