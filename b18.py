7701d01fafe64c23b0ff123a8e2e54d6db68b5dd
K AKHIL <akhilkoppala123@gmail.com>
2019-07-10 10:10:48 +0530

    Create b18.py

---
+++ b18.py
@@ -0,0 +1,15 @@
+z=input().split()
+a=int(z[0])
+b=int(z[1])
+
+k=[]
+def ams(n):
+    sum=0
+    for i in n:
+        sum=sum+(int(i)**3)
+    if(sum==int(n)):
+        return True
+for i in range(a,b+1):
+    if(ams(str(i))==True):
+        k.append(i)
+print(*k)    
