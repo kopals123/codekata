9bf8c7d40605deb9da82eef0db2cc871b856088f
K AKHIL <akhilkoppala123@gmail.com>
2019-07-10 10:28:20 +0530

    Update b20.py

--- b20.py
+++ b20.py
@@ -1,7 +1,10 @@
 z=int(input())
 b=z*5
 k=[]
-for i in range(1,b+1):
-    if(int(i)%z==0):
-        k.append(int(i))
-print(*k)
+if(z==0):
+    print("0"*5)
+else:
+    for i in range(1,b+1):
+        if(int(i)%z==0):
+            k.append(int(i))
+    print(*k)
