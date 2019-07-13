6770fab47496dcccaff3b848dd207427c47197ce
K AKHIL <akhilkoppala123@gmail.com>
2019-07-13 10:22:51 +0530

    Update h10.py

--- h10.py
+++ h10.py
@@ -1,9 +1,9 @@
-a=(input())
-b=(input())
-c=input()
-flag=0
-for i in c:
-    if i in b:
+n=input()
+a=input()
+b=input()
+for i in b:
+    flag=0
+    if( (i in a) and (a.count(i)==b.count(i))):
         flag=1
 if(flag==1):
     print("YES")
